124fba0c51136c6cf7547e75265137d0d32790e4
GustavJones <gustavjones3@gmail.com>
2023-11-16 21:59:55 +0200

    Implement Windows exist method

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -35,6 +35,19 @@ void GFiles::Directory::removeDirectory()
 
 #elif _WIN32
 #include <direct.h>
+#include <windows.h>
+
+bool GFiles::Directory::exists()
+{
+    DWORD ftyp = GetFileAttributesA(m_directoryPath->path.c_str());
+    if (ftyp == INVALID_FILE_ATTRIBUTES)
+        return false; // something is wrong with your path!
+
+    if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
+        return true; // this is a directory!
+
+    return false; // this is not a directory!
+}
 
 void GFiles::Directory::createDirectory()
 {

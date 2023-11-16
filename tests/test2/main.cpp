929dea6fb8c6405fe649fd8c59b8541da0c09fb3
GustavJones <gustavjones3@gmail.com>
2023-11-16 22:00:08 +0200

    Add exist test

--- tests/test2/main.cpp
+++ tests/test2/main.cpp
@@ -7,10 +7,11 @@ int main(int argc, char const *argv[])
 #ifdef _WIN32
     GFiles::Path p1("C:\\CXXProjects\\GFiles\\tests\\testCreate", '\\');
 #elif unix
-    GFiles::Path p1("/home/gustavjones/Documents/GFiles/tests/testCreate", '\\');
+    GFiles::Path p1("/home/gustav/Documents/GFiles/tests/testCreate", '\\');
 #endif
     GFiles::Directory d1(p1);
     // d1.createDirectory();
     d1.removeDirectory();
+    std::cout << d1.exists() << '\n';
     return 0;
 }

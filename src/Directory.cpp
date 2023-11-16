ebd3588c4386a68563141e92d0420dcaae9102d6
GustavJones <gustavjones3@gmail.com>
2023-11-16 21:53:37 +0200

    Another typo

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -14,7 +14,7 @@ bool GFiles::Directory::exists()
     struct stat st;
     if (stat(m_directoryPath->path.c_str(), &st) == 0)
     {
-        if (std.st_mode & S_IFDIR != 0)
+        if (st.st_mode & S_IFDIR != 0)
             return true;
     }
     else

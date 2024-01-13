7edb38edbb55b4db928b6d30ae614089ed15446f
GustavJones <gustavjones3@gmail.com>
2024-01-13 16:19:34 +0200

    Update create and remove methods

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -1,63 +1,55 @@
 #include "Directory.hpp"
-#include <iostream>
+#include <cstdlib>
 
 GFiles::Directory::Directory(const Path &_directoryPath)
-    : m_directoryPath(&_directoryPath)
-{
-}
+    : m_directoryPath(&_directoryPath) {}
 
 #ifdef unix
 #include <sys/stat.h>
 
-bool GFiles::Directory::exists()
-{
-    struct stat st;
-    if (stat(m_directoryPath->path.c_str(), &st) == 0)
-    {
-        if (st.st_mode & S_IFDIR != 0)
-            return true;
-    }
-    return false;
+bool GFiles::Directory::exists() {
+  struct stat st;
+  if (stat(m_directoryPath->path.c_str(), &st) == 0) {
+    if (st.st_mode & S_IFDIR != 0)
+      return true;
+  }
+  return false;
 }
 
-void GFiles::Directory::createDirectory()
-{
-    mkdir(m_directoryPath->path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+void GFiles::Directory::createDirectory() {
+  //  mkdir(m_directoryPath->path.c_str(), S_IRWXU | S_IRWXG | S_IROTH |
+  //  S_IXOTH);
+  system(("mkdir " + m_directoryPath->path).c_str());
 }
 
-void GFiles::Directory::removeDirectory()
-{
-    remove(m_directoryPath->path.c_str());
+void GFiles::Directory::removeDirectory() {
+  system(("rm -r " + m_directoryPath->path).c_str());
+  //  remove(m_directoryPath->path.c_str());
 }
 
 #elif _WIN32
 #include <direct.h>
 #include <windows.h>
 
-bool GFiles::Directory::exists()
-{
-    DWORD ftyp = GetFileAttributesA(m_directoryPath->path.c_str());
-    if (ftyp == INVALID_FILE_ATTRIBUTES)
-        return false; // something is wrong with your path!
+bool GFiles::Directory::exists() {
+  DWORD ftyp = GetFileAttributesA(m_directoryPath->path.c_str());
+  if (ftyp == INVALID_FILE_ATTRIBUTES)
+    return false; // something is wrong with your path!
 
-    if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
-        return true; // this is a directory!
+  if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
+    return true; // this is a directory!
 
-    return false; // this is not a directory!
+  return false; // this is not a directory!
 }
 
-void GFiles::Directory::createDirectory()
-{
-    _mkdir(m_directoryPath->path.c_str());
+void GFiles::Directory::createDirectory() {
+  _mkdir(m_directoryPath->path.c_str());
 }
 
-void GFiles::Directory::removeDirectory()
-{
-    _rmdir(m_directoryPath->path.c_str());
+void GFiles::Directory::removeDirectory() {
+  _rmdir(m_directoryPath->path.c_str());
 }
 
 #endif
 
-GFiles::Directory::~Directory()
-{
-}
+GFiles::Directory::~Directory() {}

397d8369cc9deeefb6f7447f7b5b22d2a573d4b0
GustavJones <gustavjones3@gmail.com>
2024-01-13 16:38:53 +0200

    Update tests

--- tests/test1/main.cpp
+++ tests/test1/main.cpp
@@ -1,16 +1,13 @@
 #include "GFiles/Path.hpp"
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
-    // Tests Path Objects
+int main(int argc, char const *argv[]) {
+  // Tests Path Objects
 
-    GFiles::Path p1("C:\\CXXProjects\\GFiles\\include", '\\');
-    GFiles::Path p2("/usr/local/games");
-    GFiles::Path p3("/usr/local/CXX a test/dir");
-    GFiles::Path p4("C:\\CXXProjects\\GFiles\\include\\Path.hpp", '\\');
-    std::cout << p1.isfile() << '\n';
-    std::cout << p4.isfile() << '\n';
-    std::cout << p4.path << '\n';
-    return 0;
+  GFiles::Path p1("C:\\CXXProjects\\GFiles\\include", '\\');
+  GFiles::Path p2("/usr/local/games");
+  GFiles::Path p3("/usr/local/CXX a test/dir");
+  GFiles::Path p4("C:\\CXXProjects\\GFiles\\include\\Path.hpp", '\\');
+  std::cout << p4.path << '\n';
+  return 0;
 }
--- tests/test2/main.cpp
+++ tests/test2/main.cpp
@@ -1,20 +1,22 @@
 #include "GFiles/Directory.hpp"
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 // Tests Directory Objects
 #ifdef _WIN32
-    GFiles::Path p1("C:\\CXXProjects\\GFiles\\tests\\testCreate", '\\');
+  GFiles::Path p1("C:\\CXXProjects\\GFiles\\tests\\testCreate", '\\');
 #elif unix
-    GFiles::Path p1("/home/gustav/Documents/GFiles/tests/testCreate", '\\');
+  GFiles::Path p1("./TestProjectasdfasdfasdfasf");
+  GFiles::Path p2(p1.path + "/src");
 #endif
-    GFiles::Directory d1(p1);
-    // d1.createDirectory();
-    d1.removeDirectory();
-    std::cout << d1.exists() << '\n';
+  GFiles::Directory d1(p1);
+  GFiles::Directory d2(p2);
+  d1.createDirectory();
+  d2.createDirectory();
+  d1.removeDirectory();
+  std::cout << d1.exists() << '\n';
 
-    std::cin.get();
+  std::cin.get();
 
-    return 0;
+  return 0;
 }
